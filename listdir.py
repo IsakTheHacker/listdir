b3475feca5aef3e7ef6cb9d4d16e0d92ac09b19a
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-03 17:49:48 +0100

    Added ability to specifiy custom path with new --path arg.

--- listdir.py
+++ listdir.py
@@ -10,13 +10,21 @@ argParser = argparse.ArgumentParser()
 argParser.add_argument("-i", "--index", help="Generate an index file containing all the files/folders in a directory.", action="store_true")
 argParser.add_argument("-n", "--nlist", help="Do not list files in console.", action="store_true")
 argParser.add_argument("-f", "--folder", help="Include folders.", action="store_true")
+argParser.add_argument("-p", "--path", help="Path to use. Will use working directory if not specified.")
 
 args = argParser.parse_args()
 
+#Set directory variable
+if args.path:
+	directory = args.path
+else:
+	directory = os.getcwd()
+
+#Open index file if --index arg is specified.
 if args.index:
 	file = open("index", "w")
 
-for entry in os.scandir(os.getcwd()):
+for entry in os.scandir(directory):
 	if entry.is_dir() and not args.folder:			#Check if entry is folder and --folder arg is not specified. Continue if
 		continue
 
